3a517a31cba58fbe32d09ed09fa9cfd6b24da64f
Rodrigo Paulino Sperandio <102339706+SperandioR@users.noreply.github.com>
2022-08-29 16:54:42 -0300

    Add files via upload

---
+++ Teste1.c
@@ -0,0 +1,16 @@
+#include <stdio.h> //comandos de entrada e saida dentro de uma biblioteca.
+
+
+// assinatura da linguagem c Inicial.
+int main()
+{
+
+    int x; // variável inteiro
+
+    x = 10; // comando de atribuição para jogar o valor dentro da variável.
+
+        printf("%d", x); // para números inteiro colocar ("%d") e a variável x.
+    return 0;
+}
+
+
