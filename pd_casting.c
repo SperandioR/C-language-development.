b0289eeacf9635826c416991d96c75361ac80131
Rodrigo Paulino Sperandio <102339706+SperandioR@users.noreply.github.com>
2022-08-30 14:44:51 -0300

    Add files via upload
    PROCESSAMENTO DE DADOS / CASTING EM C

---
+++ pd_casting.c
@@ -0,0 +1,22 @@
+#include <stdio.h> //comandos de entrada e saida dentro de uma biblioteca.
+
+int main ()
+{
+    //criação das variáveis.
+
+    int x;
+    double y;
+
+    //atribuindo valores para cada variável.
+    x = 5;
+    y = 2 * x;
+
+    //impressão dos valores.
+
+    printf("%d\n", x);
+    printf("%.1lf\n", y);
+
+
+    return 0;
+}
+
