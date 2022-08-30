2d419f842d8197977f383e9ed1c2eb8a22586f3a
Rodrigo Paulino Sperandio <102339706+SperandioR@users.noreply.github.com>
2022-08-30 12:33:21 -0300

    Add files via upload

---
+++ sainda_De_Dados_03.c
@@ -0,0 +1,24 @@
+#include <stdio.h> //comandos de entrada e saida dentro de uma biblioteca.
+#include <string.h> // Para se trabalhar com funções de string se inclui esta biblioteca.
+
+// assinatura da linguagem c Inicial.
+int main()
+{
+
+   //criação das variáveis.
+    int idade;
+    double salario;
+    char nome[50];
+    char sexo;
+
+    //atribuindo valores para cada variável.
+    idade = 32;
+    salario = 4560.9;
+    strcpy(nome, "Maria Silva");
+    sexo = 'F';
+
+    //impressão dos valores.
+    printf("A funcionaria %s, sexo %c, ganha %.2lf e tem %d anos\n", nome, sexo, salario, idade);
+
+    return 0;
+}
