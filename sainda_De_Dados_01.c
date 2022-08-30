010c2f73d535e4b33bdfc3faeed0a28839bcc5e0
Rodrigo Paulino Sperandio <102339706+SperandioR@users.noreply.github.com>
2022-08-30 12:10:59 -0300

    Add files via upload
    Estudos na linguagem C
    saida de dados

---
+++ sainda_De_Dados_01.c
@@ -0,0 +1,20 @@
+#include <stdio.h> //comandos de entrada e saida dentro de uma biblioteca.
+#include <string.h> // Para se trabalhar com funções de string se inclui esta biblioteca.
+
+// assinatura da linguagem c Inicial.
+int main()
+{
+    //criação das variáveis.
+
+    int x, y;
+
+    x = 10;
+    y = 20;
+
+    //impressão dos valores.
+
+    printf("%d\n", x);
+    printf("%d\n", y);
+
+    return 0;
+}
