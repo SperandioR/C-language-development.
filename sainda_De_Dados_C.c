7316f831d862e0d70644ff58897aada41fae9d3f
Rodrigo Paulino Sperandio <102339706+SperandioR@users.noreply.github.com>
2022-08-30 12:02:11 -0300

    Add files via upload

---
+++ sainda_De_Dados_C.c
@@ -0,0 +1,13 @@
+#include <stdio.h> //comandos de entrada e saida dentro de uma biblioteca.
+#include <string.h> // Para se trabalhar com funções de string se inclui esta biblioteca.
+
+// assinatura da linguagem c Inicial.
+int main()
+{
+    //impressão dos valores.
+
+    printf("Bom dia\n");    // (\n) barra invertido n, é o código do caracter de quebra de linha do computador.
+    printf("Boa noite\n");
+
+    return 0;
+}
