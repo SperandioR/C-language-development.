a9e7c92ad7ed259b045de6856a84e849bf18a164
Rodrigo Paulino Sperandio <102339706+SperandioR@users.noreply.github.com>
2022-09-25 18:37:44 -0300

    Add files via upload
    estrutura condicional if, else na linguagem C

---
+++ NewProject/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    //Declarando cada variável.
+    int hora;
+    printf("Digite uma hora do dia: "); //imprimindo frase para o usuário digitar algum dado.
+    scanf("%d", &hora); //fazendo a leitura dos dados inseridos pelo usuário.
+
+    if (hora < 12) //<se> a hora for menor do que 12?!
+    {
+        printf("Bom dia!\n");//imprimindo na tela (Bom dia).
+    }
+    else//<senao>...
+    {
+        printf("Boa tarde!\n");// sera imprimido na tela (Boa tarde).
+    }
+    return 0;
+}
